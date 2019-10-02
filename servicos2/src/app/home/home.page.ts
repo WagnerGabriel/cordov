26a8f0047bb5ce7a224d4a97221fa0f2d0cce02d
WagnerGabriel <44869699+WagnerGabriel@users.noreply.github.com>
2019-10-02 10:40:22 -0300

    fd

--- servicos2/src/app/home/home.page.ts
+++ servicos2/src/app/home/home.page.ts
@@ -9,4 +9,13 @@ export class HomePage {
 
   constructor() {}
 
+  teste = [
+    {nome: "A1", checado: false},
+    {nome: "A2", checado: false},
+    {nome: "A3", checado: false},
+    {nome: "A4", checado: false},
+    {nome: "A5", checado: false},
+    {nome: "A6", checado: false},
+    {nome: "A7", checado: false},
+  ]
 }
